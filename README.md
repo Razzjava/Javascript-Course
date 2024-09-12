2855173e6ae7f257a0065835cd50395fa2452325
Raz <33026305+Razzjava@users.noreply.github.com>
2024-09-12 12:02:46 +0100

    episode 1:variables

---
+++ README.md
@@ -0,0 +1,6 @@
+# JavaScript Course
+
+Welcome to a Course covering the fundamentals of Javascript, as this course goes on you will be able to view more files related to the 
+videos we have covered.
+
+please be patient as this is all done in my spare time and I will try to upload as fast as possible
---
+++ Variables.js
@@ -0,0 +1,38 @@
+
+// Variables
+
+// old way of declaring a variable
+var name ="razz"
+
+// let can be updated
+let name2 = "razz"
+
+console.log(name2)
+
+name2 = "james"
+console.log(name2)
+
+// const cannot be updated
+const name3 = "razz"
+
+// Data Types
+
+// String: are text, wrapped in quotes
+let lastName = "smith"
+console.log(typeof lastName)
+
+//number: can be integers, decimals, and more
+let age = 28
+console.log(typeof age)
+
+//boolean:  true or false
+let isSubscribed = false
+console.log(typeof isSubscribed)
+
+//undefined: means the variable has been declared but not given a value
+let hello;
+console.log(typeof hello)
+
+// null type: represents an empty value on purpose
+let empty = null;
+console.log(typeof empty)
